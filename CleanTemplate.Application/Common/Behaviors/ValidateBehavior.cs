fdc8305c7590dcd547fa080f9d6dab25bcb52f3e
Yasser Bouaabane <b.yasser@Alexsys.solutions>
2022-09-30 15:35:33 +0100

    Query validator

---
+++ CleanTemplate.Application/Common/Behaviors/ValidateBehavior.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using ErrorOr;
+using FluentValidation;
+
+namespace CleanTemplate.Application.Common.Behaviors;
+
+public class ValidateBehavior<TRequest,TResponse> 
+    : IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidateBehavior(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        CancellationToken cancellationToken,
+        RequestHandlerDelegate<TResponse> next)
+    {        
+        // Before handler
+        if (_validator is null)
+            return await next();
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (validationResult.IsValid)
+            return await next();
+        
+        
+        // After the handler
+        var errors = validationResult.Errors
+            .ConvertAll(
+                validationFailure =>
+                    Error.Validation(
+                        validationFailure.PropertyName,
+                        validationFailure.ErrorMessage
+                    )
+            );
+        return (dynamic)errors;
+    }
+}
--- CleanTemplate.Application/DependencyInjection.cs
+++ CleanTemplate.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using CleanTemplate.Application.Common.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 using MediatR;
+using FluentValidation;
 
 namespace CleanTemplate.Application
 {
@@ -8,6 +11,11 @@ namespace CleanTemplate.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(typeof(DependencyInjection).Assembly);
+            services.AddScoped(
+                typeof(IPipelineBehavior<, >),
+                typeof(ValidateBehavior<, >)
+                );
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             return services;
 
         }
