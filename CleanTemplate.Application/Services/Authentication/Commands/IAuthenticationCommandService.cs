f5a65d9c508c245e5d5454bd2741d070c6977431
Yasser Bouaabane <b.yasser@Alexsys.solutions>
2023-03-16 11:18:30 +0100

    ..

--- CleanTemplate.Application/Services/Authentication/Commands/IAuthenticationCommandService.cs
+++ CleanTemplate.Application/Services/Authentication/Commands/IAuthenticationCommandService.cs
@@ -1,10 +1,9 @@
 ﻿using CleanTemplate.Application.Services.Authentication.Common;
 using ErrorOr;
 
-namespace CleanTemplate.Application.Services.Authentication.Commands
+namespace CleanTemplate.Application.Services.Authentication.Commands;
+
+public interface IAuthenticationCommandService
 {
-    public interface IAuthenticationCommandService
-    {
-        ErrorOr<AuthenticationResult> Register(string firstName, string lastName, string email, string password);
-    }
+    ErrorOr<AuthenticationResult> Register(string firstName, string lastName, string email, string password);
 }
